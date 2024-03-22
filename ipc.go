66b3c63bbd62c5f2f884dd822f37d98bb5452e71
daniel <danielhookx@gmail.com>
2024-03-22 18:10:00 +0800

    complete netbus subscribe sync and unsubscribe

--- ipc.go
+++ ipc.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/rpc"
 	"net/url"
+	"sync"
 )
 
 type SubArgs struct {
@@ -15,6 +16,15 @@ type SubArgs struct {
 }
 
 type SubReply struct {
+	Handle uintptr
+}
+
+type UnsubArgs struct {
+	Topic  string
+	Handle uintptr
+}
+
+type UnsubReply struct {
 }
 
 type PubArgs struct {
@@ -26,9 +36,11 @@ type PubReply struct {
 }
 
 type RPCProxy struct {
-	rawURL    string
-	remoteURL string
-	bus       Eventbus
+	sync.Mutex
+	rawURL       string
+	remoteURL    string
+	bus          Eventbus
+	local2Remote map[uintptr]uintptr
 }
 
 func NewRPCProxyCreator(rawURL, remoteURL string) ProxyCreator {
@@ -43,9 +55,10 @@ func NewRPCProxyCreator(rawURL, remoteURL string) ProxyCreator {
 
 func NewRPCProxy(rawURL, remoteURL string, bus Eventbus) (*RPCProxy, error) {
 	p := &RPCProxy{
-		rawURL:    rawURL,
-		remoteURL: remoteURL,
-		bus:       bus,
+		rawURL:       rawURL,
+		remoteURL:    remoteURL,
+		bus:          bus,
+		local2Remote: make(map[uintptr]uintptr),
 	}
 	gob.Register([]interface{}{})
 	rpc.Register(p)
@@ -76,7 +89,7 @@ func (p *RPCProxy) Subscribe(topic string, fn interface{}) error {
 		return fmt.Errorf("Client connection error %w", err)
 	}
 
-	reply := &SubReply{}
+	reply := SubReply{}
 	err = client.Call("RPCProxy.RPCSubscribe", &SubArgs{
 		RemoteURL: p.rawURL,
 		Topic:     topic,
@@ -84,6 +97,9 @@ func (p *RPCProxy) Subscribe(topic string, fn interface{}) error {
 	if err != nil {
 		return fmt.Errorf("Client invocation error: %w", err)
 	}
+	p.Lock()
+	p.local2Remote[functionWrapper(fn)] = reply.Handle
+	p.Unlock()
 	// Call the local subscription method and register the callback locally
 	return p.bus.Subscribe(topic, fn)
 }
@@ -107,12 +123,19 @@ func (p *RPCProxy) SubscribeSync(topic string, fn interface{}) error {
 	if err != nil {
 		return fmt.Errorf("Client invocation error: %w", err)
 	}
+	p.Lock()
+	p.local2Remote[functionWrapper(fn)] = reply.Handle
+	p.Unlock()
 	// Call the local subscription method and register the callback locally
 	return p.bus.SubscribeSync(topic, fn)
 }
 
 func (p *RPCProxy) Unsubscribe(topic string, handler interface{}) error {
-	// Call the remote subscription method to register the event to the remote endpoint.
+	p.Lock()
+	handle := p.local2Remote[functionWrapper(handler)]
+	p.Unlock()
+
+	// Call the remote unsubscription method to remove the event.
 	remote, err := url.Parse(p.remoteURL)
 	if err != nil {
 		return fmt.Errorf("Parse remote url error: %w", err)
@@ -122,15 +145,19 @@ func (p *RPCProxy) Unsubscribe(topic string, handler interface{}) error {
 		return fmt.Errorf("Client connection error %w", err)
 	}
 
-	reply := &SubReply{}
-	err = client.Call("RPCProxy.RPCSubscribeSync", &SubArgs{
-		RemoteURL: p.rawURL,
-		Topic:     topic,
+	reply := &UnsubReply{}
+	err = client.Call("RPCProxy.RPCUnsubscribe", &UnsubArgs{
+		Topic:  topic,
+		Handle: handle,
 	}, &reply)
 	if err != nil {
 		return fmt.Errorf("Client invocation error: %w", err)
 	}
-	// Call the local subscription method and register the callback locally
+
+	p.Lock()
+	delete(p.local2Remote, functionWrapper(handler))
+	p.Unlock()
+	// Call the local unsubscription method and remove the callback locally
 	return p.bus.Unsubscribe(topic, handler)
 }
 
@@ -141,24 +168,22 @@ func (p *RPCProxy) Publish(topic string, args ...interface{}) {
 func (p *RPCProxy) RPCSubscribe(args *SubArgs, reply *SubReply) error {
 	// Receive subscription method calls from the peer
 	// callback method actually executes the remote call of Publish
-	cb := p.doSubscribeCallback(args, reply)
-	p.bus.Subscribe(args.Topic, cb)
-	return nil
+	cb := p.doSubscribeCallback(args)
+	reply.Handle = functionWrapper(cb)
+	return p.bus.Subscribe(args.Topic, cb)
 }
 
 func (p *RPCProxy) RPCSubscribeSync(args *SubArgs, reply *SubReply) error {
 	// Receive subscription method calls from the peer
 	// callback method actually executes the remote call of Publish
-	cb := p.doSubscribeCallback(args, reply)
+	cb := p.doSubscribeCallback(args)
+	reply.Handle = functionWrapper(cb)
 	p.bus.SubscribeSync(args.Topic, cb)
 	return nil
 }
 
-func (p *RPCProxy) RPCUnsubscribe(args *SubArgs, reply *SubReply) error {
-	// Receive subscription method calls from the peer
-	// callback method actually executes the remote call of Publish
-	cb := p.doSubscribeCallback(args, reply)
-	p.bus.Unsubscribe(args.Topic, cb)
+func (p *RPCProxy) RPCUnsubscribe(args *UnsubArgs, reply *UnsubReply) error {
+	p.bus.Unsubscribe(args.Topic, args.Handle)
 	return nil
 }
 
@@ -170,7 +195,7 @@ func (p *RPCProxy) RPCPublish(args *PubArgs, reply *PubReply) error {
 
 type subscribeCallback func(data any) error
 
-func (p *RPCProxy) doSubscribeCallback(args *SubArgs, reply *SubReply) subscribeCallback {
+func (p *RPCProxy) doSubscribeCallback(args *SubArgs) subscribeCallback {
 	return func(data any) error {
 		remote, err := url.Parse(args.RemoteURL)
 		if err != nil {
