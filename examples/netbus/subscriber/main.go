90069e96c591bc214ca197b314b75880d2546c1f
daniel <danielhookx@gmail.com>
2024-03-23 01:55:18 +0800

    netbus example complate

--- examples/netbus/subscriber/main.go
+++ examples/netbus/subscriber/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 
 	eventbus "github.com/danielhookx/eventbus"
 )
@@ -10,12 +15,95 @@ func SayHello(name string) {
 	fmt.Printf("hello %s\n", name)
 }
 
+func SayHiDely(name string) {
+	time.Sleep(1 * time.Second * 2)
+	fmt.Printf("hi %s\n", name)
+}
+
 func main() {
 	rawURL := "tcp://:7634"
 	remoteURL := "tcp://localhost:7633"
 
 	bus := eventbus.NewEventBus(eventbus.WithProxys(eventbus.NewRPCProxyCreator(rawURL, remoteURL)))
-	bus.Subscribe("test-hello", SayHello)
 
-	select {}
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Note: All input ends with `Enter`, type `exit` return to upper level")
+
+	topic := "test"
+
+	for {
+		fmt.Println("Input topic:")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("input with error", err)
+			return
+		}
+		input = strings.TrimRight(input, "\n")
+		if input == "exit" {
+			break
+		}
+		topic = input
+		if topic == "" {
+			fmt.Println("topic can not be empty")
+			continue
+		}
+		fmt.Printf("topic is: %s\n", topic)
+		for {
+			fmt.Println(`choose order type:
+			1. subscribe
+			2. sync subscribe
+			3. unsubscribe
+			`)
+			input, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("input with error", err)
+				return
+			}
+			commandTypeStr := strings.TrimRight(input, "\n")
+			if commandTypeStr == "exit" {
+				break
+			}
+
+			command, err := strconv.Atoi(commandTypeStr)
+			if err != nil {
+				fmt.Println("unsupported command type %v", commandTypeStr)
+				continue
+			}
+
+			fmt.Println(`choose method type:
+			1. SayHello
+			2. SayHiDely
+			`)
+			input, err = reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("input with error", err)
+				return
+			}
+			methodStr := strings.TrimRight(input, "\n")
+
+			method, err := strconv.Atoi(methodStr)
+			if err != nil {
+				fmt.Println("unsupported method type %v", methodStr)
+				continue
+			}
+
+			var fn func(string)
+			switch method {
+			case 1:
+				fn = SayHello
+			case 2:
+				fn = SayHiDely
+			}
+
+			switch command {
+			case 1:
+				bus.Subscribe(topic, fn)
+			case 2:
+				bus.Subscribe(topic, fn)
+			case 3:
+				bus.Unsubscribe(topic, fn)
+			}
+		}
+	}
+	fmt.Println("exit")
 }
